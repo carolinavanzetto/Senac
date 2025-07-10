7fc898a6d392efd7827bcd9ffaa18ffbd2cc0129
carolinavanzetto <caroli.vanzeto@gmail.com>
2025-07-10 08:49:10 -0300

    Add files via upload

---
+++ variaveis.py
@@ -0,0 +1,10 @@
+print('Variaveis')
+print('Bom dia '+nome)
+nome = 'Carolina'
+print(nome)
+idade = 16
+salario_minimo = 1450.99
+print(salario_minimo)
+ativo = True
+print(ativo)
+print(type(nome))
